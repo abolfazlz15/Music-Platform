88edf5dfa9648fb2b4d317eefaa574c8af848919
Abolfazl <abolfazlzaker1234@gmail.com>
2023-02-07 14:15:55 +0330

    add OTP class

---
+++ accounts/otp_service.py
@@ -0,0 +1,51 @@
+import datetime
+from random import randint
+
+from django.core.cache import cache
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+
+class OTP:
+
+    def __init__(self):
+        self.otp_expiry_minutes = 1
+
+    def generate_otp(self, email):
+        otp = randint(1000, 9999)
+        cache.set(email, (otp, datetime.datetime.now()), self.otp_expiry_minutes * 60)
+        # self.send_otp(otp, email)
+        print(otp)
+
+        return otp
+
+    def verify_otp(self, otp, email):
+        stored_otp_info = cache.get(email)
+        if stored_otp_info is None:
+            return False
+        stored_otp, timestamp = stored_otp_info
+        if stored_otp == int(otp):
+            if datetime.datetime.now() - timestamp > datetime.timedelta(minutes=self.otp_expiry_minutes):
+                self.clear_otp(email)
+                return False
+            else:
+                self.clear_otp(email)
+                return True
+        else:
+            return False
+
+    def clear_otp(self, email):
+        cache.delete(email)
+
+    def send_otp(self, otp, email):
+        pass
+        # mail_subject = 'فعال سازی اکانت'
+        # message = render_to_string('accounts/active_email.html', {
+        #     'user': email,
+        #     'code': otp,
+        # })
+        # to_email = email
+        # email = EmailMessage(
+        #     mail_subject, message, to=[to_email]
+        # )
+        # email.send()

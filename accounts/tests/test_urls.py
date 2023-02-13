71f6749f5a39dfc49ac82070af14e4a5c3849597
Abolfazl <abolfazlzaker1234@gmail.com>
2023-02-13 21:16:02 +0330

    add test for acconts app  urls

---
+++ accounts/tests/test_urls.py
@@ -0,0 +1,37 @@
+from django.urls import resolve, reverse
+from rest_framework.test import APITestCase
+
+from accounts.api import views
+from accounts.models import User
+
+class TestUrls(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            email='test@gmail.com', username='test', password='test1234')
+
+
+    def test_login(self):
+        url = reverse('accounts:login')
+        self.assertEqual(resolve(url).func.view_class, views.UserLoginView)
+
+    def test_register(self):
+        url = reverse('accounts:register')
+        self.assertEqual(resolve(url).func.view_class, views.UserRegisterView)
+
+    def test_register_check_otp(self):
+        url = reverse('accounts:check-otp')
+        self.assertEqual(resolve(url).func.view_class, views.GetOTPRegisterCodeView)
+        
+    def test_user_profile(self):
+
+        url = reverse('accounts:profile', args=(self.user.id,))
+        self.assertEqual(resolve(url).func.view_class, views.UserProfileView)
+
+    def test_user_profile_update(self):
+        url = reverse('accounts:profile-update')
+        self.assertEqual(resolve(url).func.view_class, views.UserUpdateProfileView)
+
+    def test_user_password_update(self):
+        url = reverse('accounts:password-update')
+        self.assertEqual(resolve(url).func.view_class, views.ChangePasswordView)

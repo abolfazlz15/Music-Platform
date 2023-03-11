cba797f2f3ebbafd893edb63dca094c41ccbb6f7
Abolfazl <abolfazlzaker.dev@gmail.com>
2023-03-11 13:29:44 +0330

    test: add test for UserRegisterView

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,10 +1,14 @@
+from unittest.mock import MagicMock
+
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-User = get_user_model()
+from accounts.otp_service import OTP
 
+
+User = get_user_model()
 
 class UserLoginViewTestCase(APITestCase):
     def setUp(self):
@@ -44,3 +48,38 @@ class UserLoginViewTestCase(APITestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         
+
+class UserRegisterViewTestCase(APITestCase):
+    def setUp(self):
+        self.url = reverse('accounts:register')
+        self.valid_payload = {
+            'email': 'testuser@example.com',
+            'username': 'testuser',
+            'password': 'testpassword123'
+        }
+        self.invalid_payload = {
+            'email': 'invalidemail',
+            'username': '',
+            'password': ''
+        }
+
+def test_register_user_with_valid_payload(self):
+    mock_otp_generate = OTP.generate_otp = MagicMock(return_value='123456')
+    response = self.client.post(self.url, self.valid_payload, format='json')
+    self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+    self.assertEqual(mock_otp_generate.call_count, 1)
+
+    def test_register_user_with_invalid_payload(self):
+        response = self.client.post(self.url, self.invalid_payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+
+    def test_register_user_with_missing_fields(self):
+        response = self.client.post(self.url, {'email': 'test@example.com'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+
+    def test_register_user_with_existing_email(self):
+        # Create a user with the same email as in the valid payload
+        User.objects.create_user(email=self.valid_payload['email'], username='existinguser', password='existingpassword')
+        response = self.client.post(self.url, self.valid_payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+        self.assertEqual(response.data['email'][0], 'user with this email already exists.')

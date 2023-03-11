75942eca1463ce2e772ae14ef7ee1fc2efa175bb
Abolfazl <abolfazlzaker.dev@gmail.com>
2023-03-11 15:41:56 +0330

    test: add ArtistProfileViewTestCase and UserProfileViewTestCase & fix some bug in UserLoginViewTestCase

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -4,14 +4,16 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
 
+from accounts.models import Artist
 from accounts.otp_service import OTP
-
 
 User = get_user_model()
 
 class UserLoginViewTestCase(APITestCase):
     def setUp(self):
+        self.url = reverse('accounts:login')
         self.user = User.objects.create_user(
             email='test@example.com',
             username='test',
@@ -29,9 +31,8 @@ class UserLoginViewTestCase(APITestCase):
         }
 
     def test_login_with_valid_credentials(self):
-        url = reverse('accounts:login')
         response = self.client.post(
-            url,
+            self.url,
             data=self.valid_payload,
             format='json'
         )
@@ -40,9 +41,8 @@ class UserLoginViewTestCase(APITestCase):
         self.assertIn('access', response.data)
 
     def test_login_with_invalid_credentials(self):
-        url = reverse('accounts:login')
         response = self.client.post(
-            url,
+            self.url,
             data=self.invalid_payload,
             format='json'
         )
@@ -63,11 +63,11 @@ class UserRegisterViewTestCase(APITestCase):
             'password': ''
         }
 
-def test_register_user_with_valid_payload(self):
-    mock_otp_generate = OTP.generate_otp = MagicMock(return_value='123456')
-    response = self.client.post(self.url, self.valid_payload, format='json')
-    self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
-    self.assertEqual(mock_otp_generate.call_count, 1)
+    def test_register_user_with_valid_payload(self):
+        mock_otp_generate = OTP.generate_otp = MagicMock(return_value='123456')
+        response = self.client.post(self.url, self.valid_payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(mock_otp_generate.call_count, 1)
 
     def test_register_user_with_invalid_payload(self):
         response = self.client.post(self.url, self.invalid_payload, format='json')
@@ -77,9 +77,36 @@ def test_register_user_with_valid_payload(self):
         response = self.client.post(self.url, {'email': 'test@example.com'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
 
-    def test_register_user_with_existing_email(self):
-        # Create a user with the same email as in the valid payload
-        User.objects.create_user(email=self.valid_payload['email'], username='existinguser', password='existingpassword')
-        response = self.client.post(self.url, self.valid_payload, format='json')
-        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
-        self.assertEqual(response.data['email'][0], 'user with this email already exists.')
+
+
+class UserProfileViewTestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.email = 'test@test.com'
+        cls.username = 'test'
+        cls.password = 'test1234'
+        cls.user = User.objects.create_user(email='test@test.com', username='test', password='test1234')
+        refresh = RefreshToken.for_user(cls.user)
+        cls.token = str(refresh.access_token)
+
+    def test_get_profile_user(self):
+        url = reverse('accounts:profile', args=(self.user.id,))
+        response = self.client.get(url, HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class ArtistProfileViewTestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.email = 'test@test.com'
+        cls.username = 'test'
+        cls.password = 'test1234'
+        cls.user = User.objects.create_user(email='test@test.com', username='test', password='test1234')
+        cls.artist = Artist.objects.create(name='testArtist')
+        refresh = RefreshToken.for_user(cls.user)
+        cls.token = str(refresh.access_token)
+
+    def test_get_profile_user(self):
+        url = reverse('accounts:profile-artist', args=(self.artist.id,))
+        response = self.client.get(url, HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

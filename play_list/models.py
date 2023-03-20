d88285e8b5e408e4d1f482dfc101d47a9ffefc57
Abolfazl <abolfazlzaker.dev@gmail.com>
2023-03-20 13:06:20 +0330

    test:add test folder for playlist app & add test for play_lsit model

--- play_list/models.py
+++ play_list/models.py
@@ -8,13 +8,11 @@ from music.models import Music
 class Playlist(models.Model):
     name = models.CharField(max_length=100, verbose_name=_('name'))
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='playlists', verbose_name=_('user'))
-    songs = models.ManyToManyField(Music, related_name='playlists', verbose_name=_('sangs'))
+    songs = models.ManyToManyField(Music, related_name='playlists', verbose_name=_('songs'))
 
     class Meta:
         verbose_name = 'پلی لیست'
         verbose_name_plural = 'پلی لیست ها'
 
-
     def __str__(self):
         return self.name
-    

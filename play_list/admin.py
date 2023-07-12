9ee0d9465f48b373571cc7b830618165921dec25
abolfazl <abolfazlzaker1234@gmail.com>
2023-07-12 21:21:41 +0330

    feat: add approved playlist API

--- play_list/admin.py
+++ play_list/admin.py
@@ -4,10 +4,10 @@ from play_list.models import Playlist
 @admin.register(Playlist)
 class PlaylistAdmin(admin.ModelAdmin):
     # for show customization list objects
-    fields = ('name', 'user', 'songs')
-    list_display = ('name', 'user')
+    fields = ('name', 'user', 'songs', 'approved_playlist')
+    list_display = ('name', 'user', 'approved_playlist')
     search_fields = ('user', 'name')
-    list_display = ('name', 'user')
     filter_horizontal = ['songs']
     list_select_related = ['user']
     list_per_page = 50
+    list_filter = ['approved_playlist']

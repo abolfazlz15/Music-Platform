b95dbb8f53d49cd70ac4e4e250974ffa1fc20ef0
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-08-29 18:28:13 +0330

    feat: add approved playlist model

--- play_list/models.py
+++ play_list/models.py
@@ -7,14 +7,27 @@ from music.models import Music
 
 class Playlist(models.Model):
     name = models.CharField(max_length=100, verbose_name=_('name'))
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='playlists', verbose_name=_('user'), null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='playlists', verbose_name=_('user'))
     songs = models.ManyToManyField(Music, related_name='playlists', blank=True, verbose_name=_('songs'))
-    approved_playlist = models.BooleanField(default=False)
-    is_international = models.BooleanField(null=True, blank=True)
+
 
     class Meta:
         verbose_name = 'پلی لیست'
         verbose_name_plural = 'پلی لیست ها'
+
+    def __str__(self):
+        return self.name
+
+
+class ApprovedPlaylist(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_('name'))
+    songs = models.ManyToManyField(Music, related_name='approved_playlists', blank=True, verbose_name=_('songs'))
+    cover = models.ImageField(upload_to='image/palylist/cover')
+    is_international = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'پلی لیست عمومی'
+        verbose_name_plural = 'پلی لیست های عمومی'
 
     def __str__(self):
         return self.name

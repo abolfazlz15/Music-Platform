ac3099f6fe19491ae2e8f8afc8d6eb1dec478829
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-09-07 21:17:17 +0330

    fix: change page_size for pagination

--- music/pagination.py
+++ music/pagination.py
@@ -2,5 +2,5 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 10
     page_query_param = 'page_number'

030fcdbbfd3fbce3d697ae66d2e16cfa721d5b5b
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-11-25 11:37:06 +0330

    feat: add middleware for get client IP Address

--- music_player/settings.py
+++ music_player/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'music.middleware.SaveIPAddressMiddleware',
 ]
 
 

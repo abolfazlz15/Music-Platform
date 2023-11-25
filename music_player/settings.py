6178769a92df1200614d5ed385a3b28aa1a1f375
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-11-25 16:30:56 +0330

    feat: get user ip and add add_view_music method

--- music_player/settings.py
+++ music_player/settings.py
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'music.middleware.SaveIPAddressMiddleware',
 ]
 
 

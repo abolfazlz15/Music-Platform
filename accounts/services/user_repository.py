bea664caf3a5fc9e0daf609863319ee91df7b0e7
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2024-08-30 00:11:06 +0330

    refactor: add base of user repository

---
+++ accounts/services/user_repository.py
@@ -0,0 +1,17 @@
+
+class UserRepository:
+    @staticmethod
+    def get_user_by_id(user_id: int):
+        pass
+
+    @staticmethod
+    def create_user(*, email: str, username: str):
+        pass
+
+    @staticmethod
+    def update_user(*, user_id: int, email: str, username: str):
+        pass
+
+    @staticmethod
+    def delete_user(*, user_id: int):
+        pass
